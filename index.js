e9a03fd2a1dbf4ac4d89cc70dc1867e79cfd3619
Atanas Atanasov <scriptex.bg@gmail.com>
2018-03-09 11:29:06 +0200

    Add first version

---
+++ index.js
@@ -0,0 +1,15 @@
+const randomSplice = arr => {
+	if (!Array.isArray(arr)) {
+		throw new TypeError('Parameter 1 should be of type Array');
+	}
+
+	const len = arr.length;
+
+	if (!len) {
+		throw new Error(`No items in the array`);
+	}
+
+	const randomIndex = Math.floor(Math.random() * len);
+
+	return arr.splice(randomIndex, 1)[0];
+};
