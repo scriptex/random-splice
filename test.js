c9bf5f0eb5ddad06e6893e1db4d18ae5d5feefd9
Atanas Atanasov <scriptex.bg@gmail.com>
2018-03-09 15:26:57 +0200

    Add tests

---
+++ test.js
@@ -0,0 +1,27 @@
+const tape = require('tape');
+const randomSplice = require('./');
+
+tape('should throw TypeError', t => {
+	t.throws(() => randomSplice(null));
+	t.throws(() => randomSplice({}));
+	t.throws(() => randomSplice(undefined));
+	t.throws(() => randomSplice(''));
+	t.throws(() => randomSplice(1));
+	t.end();
+});
+
+tape('should throw Error', t => {
+	t.throws(() => randomSplice([]));
+	t.end();
+});
+
+tape('should remove from array', t => {
+	const tmp = ['Item', 'Test', 'Thing', 'Example'];
+
+	while (tmp.length) {
+		randomSplice(tmp);
+	}
+
+	t.equal(0, tmp.length);
+	t.end();
+});
